555511ede2c88fe13129b0b27c60b2915386951c
jaime.hyland@language-landscapes.com <james.hyland@iubh.de>
2026-02-09 11:49:32 +0100

    Add console.log next-config to check base url is being read.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from "next";
 
+console.log(
+  "[next.config] NEXT_PUBLIC_API_BASE_URL =",
+  process.env.NEXT_PUBLIC_API_BASE_URL
+);
 
 const NEXT_PUBLIC_API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
